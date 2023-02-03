47f9f7a580f50a92c95d48fe9d11094579fb29e7
Vladimir Tselobanov <118988749+VladimirTz@users.noreply.github.com>
2023-02-03 18:33:29 +0300

    Update Script.sql
    update 1

--- Script.sql
+++ Script.sql
@@ -1,15 +1,11 @@
 /*1. Создать таблицу employees
 - id. serial,  primary key,
-- employee_name. Varchar(50), not null
-*/
-
+- employee_name. Varchar(50), not null*/
 create table employees(
 	id serial  primary key,
-	employee_name Varchar(50) not null
-);
+	employee_name Varchar(50) not null);
 
 /*2. Наполнить таблицу employee 70 строками.*/
-
 insert into employees (employee_name)
 values  ('Vladimir'),
 		('Clara Poore'), 
@@ -85,11 +81,9 @@ values  ('Vladimir'),
 /*3. Создать таблицу salary
 - id. Serial  primary key,
 - monthly_salary. Int, not null*/	
-
 create table salary(
 	id serial primary key,
-	monthly_salary int not null
-);
+	monthly_salary int not null);
 
 /*4. Наполнить таблицу salary 15 строками:
 - 1000
@@ -107,11 +101,9 @@ create table salary(
 - 2200
 - 2300
 - 2400
-- 2500
-*/
-
+- 2500*/
 insert into salary (monthly_salary)
-values  (1000),
+values  	(1000),
 		(1100),
 		(1200),
 		(1300),
@@ -131,22 +123,17 @@ values  (1000),
 /*5. Создать таблицу employee_salary
 - id. Serial  primary key,
 - employee_id. Int, not null, unique
-- salary_id. Int, not null
-*/	
-	
+- salary_id. Int, not null*/		
 create table employee_salary(
 	id serial  primary key,
 	employee_id int not null unique,
-	salary_id int not null
-);	
+	salary_id int not null);	
 
 /*6. Наполнить таблицу employee_salary 40 строками:
-- в 10 строк из 40 вставить несуществующие employee_id
-*/	
-
+- в 10 строк из 40 вставить несуществующие employee_id*/	
 insert into employee_salary (employee_id, salary_id)
 values (70, 16),
-   	   (60, 14),
+       (60, 14),
        (50, 12),
        (40, 10),
        (30, 8),
@@ -188,19 +175,15 @@ values (70, 16),
 
 /*7. Создать таблицу roles
 - id. Serial  primary key,
-- role_name. int, not null, unique
-*/	
+- role_name. int, not null, unique*/	
 create table roles(
 	id serial primary key,
-	role_name int not null unique
-	);
+	role_name int not null unique);
 
 /*8. Поменять тип столба role_name с int на varchar(30)*/	
-
 alter table roles alter column role_name type varchar(30);
 
 /*9. Наполнить таблицу roles 20 строками*/
-
 insert into roles (role_name)
 values ('Junior Python developer'),
 		('Middle Python developer'),
@@ -226,9 +209,7 @@ values ('Junior Python developer'),
 /*10. Создать таблицу roles_employee
 - id. Serial  primary key,
 - employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
-- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
-*/	
-	
+- role_id. Int, not null (внешний ключ для таблицы roles, поле id)*/		
 create table roles_employee(
 	id serial primary key,
 	employee_id Int not null unique,
@@ -236,14 +217,12 @@ create table roles_employee(
 		foreign key (employee_id)
 			references employees(id),
 		foreign key (role_id)
-			references roles(id)
-); 
+			references roles(id)); 
 
 /*11. Наполнить таблицу roles_employee 40 строками*/
-
 insert into roles_employee(employee_id, role_id)
 values (40, 1),
-   	   (38, 3),
+       (38, 3),
        (36, 5),
        (34, 7),
        (32, 9),
